8c0eaf9ac7a0f2e2553082592a8482ce0e4fe59b
Rachel Wall <solistra@gmx.com>
2014-08-19 23:12:45 -0400

    Further improve installation.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in $(pwd)/files/*; do
+for i in $(dirname $(readlink -f $0))/files/*; do
   echo "Installing $(basename $i)..."
   rm -rf ~/.$(basename $i)
   ln -s $i ~/.$(basename $i)
