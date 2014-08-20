fce603d0fa69fafe9a26cf1099bec1850fa7b5a6
Rachel Wall <solistra@gmx.com>
2014-08-19 23:02:41 -0400

    Make installation more dynamic.

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-for i in ~/.dotfiles/files/*; do
+for i in $(pwd)/files/*; do
   echo "Installing $(basename $i)..."
   rm -rf ~/.$(basename $i)
   ln -s $i ~/.$(basename $i)
 done
+
+echo "Done."
