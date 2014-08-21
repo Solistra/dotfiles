f4f959f623a28c4acba2efc2c76bd771514e100e
Rachel Wall <solistra@gmx.com>
2014-08-20 21:41:59 -0400

    Use tabs instead of spaces.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 for i in $(dirname $(readlink -f $0))/files/*; do
-  echo "Installing $(basename $i)..."
-  rm -rf ~/.$(basename $i)
-  ln -s $i ~/.$(basename $i)
+	echo "Installing $(basename $i)..."
+	rm -rf ~/.$(basename $i)
+	ln -s $i ~/.$(basename $i)
 done
 
 echo "Done."
