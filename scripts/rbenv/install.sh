e0764499d0e1ac3cbc6585de17ac4d724e59094f
Rachel Wall <solistra@gmx.com>
2014-09-09 15:02:11 -0400

    Generalize install location for rbenv.

--- scripts/rbenv/install.sh
+++ scripts/rbenv/install.sh
@@ -9,11 +9,11 @@
 # status, and exits with either 0 for success or 1 for failure.
 report_status(){
 	if [ $? -eq 0 ] ; then
-		echo 'rbenv and ruby-build installed to ~/.rbenv/bin/rbenv.'
+		echo "rbenv and ruby-build installed to $loc."
 		exit 0
 	else
 		echo 'rbenv and ruby-build failed to install.'	
-		echo 'Check for remnants in ~/.rbenv.'
+		echo "Check for remnants in $loc."
 		exit 1
 	fi
 }
@@ -22,25 +22,27 @@ report_status(){
 #   Provides automated installation for the `rbenv` command and `ruby-build`
 # plugin.
 scripted_install(){
-	if [ -d $HOME/.rbenv ] ; then
-		read -p "$HOME/.rbenv exists. Overwrite? (y/n) " input
+	if [ -d $loc ] ; then
+		read -p "$loc exists. Overwrite? (y/n) " input
 		[ "$input" != 'y' ] && return 2
-		rm -rf $HOME/.rbenv
+		rm -rf $loc
 	fi
 	[ -x "$(which git)" ] || sudo apt-get install git
 	echo 'Cloning rbenv source repository...'
-	git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
+	git clone https://github.com/sstephenson/rbenv.git $loc
 	echo 'Cloning ruby-build source repository...'
 	git clone https://github.com/sstephenson/ruby-build.git \
-		$HOME/.rbenv/plugins/ruby-build
+		"$loc/plugins/ruby-build"
 }
 # =============================================================================
 # Script Body
 # =============================================================================
 echo 'Installing rbenv and ruby-build...'
 if [ -x "$(which brew)" ] ; then
+	loc='/usr/local/opt/rbenv'
 	brew update && brew install rbenv ruby-build
 else
+	loc="$HOME/.rbenv"
 	scripted_install
 fi
 report_status
