e1059c8fc7d690be9d079c5c840cf2dfcd48aa91
Rachel Wall <solistra@gmx.com>
2014-09-08 21:24:10 -0400

    Allow rbenv install script to overwrite existing ~/.rbenv directory.

--- scripts/rbenv/install.sh
+++ scripts/rbenv/install.sh
@@ -9,10 +9,11 @@
 # status, and exits with either 0 for success or 1 for failure.
 report_status(){
 	if [ $? -eq 0 ] ; then
-		echo 'rbenv installed to ~/.rbenv/bin/rbenv.'
+		echo 'rbenv and ruby-build installed to ~/.rbenv/bin/rbenv.'
 		exit 0
 	else
-		echo 'rbenv failed to install.'	
+		echo 'rbenv and ruby-build failed to install.'	
+		echo 'Check for remnants in ~/.rbenv.'
 		exit 1
 	fi
 }
@@ -21,6 +22,11 @@ report_status(){
 #   Provides automated installation for the `rbenv` command and `ruby-build`
 # plugin.
 scripted_install(){
+	if [ -d $HOME/.rbenv ] ; then
+		read -p "$HOME/.rbenv exists. Overwrite? (y/n) " input
+		[ "$input" != 'y' ] && return 2
+		rm -rf $HOME/.rbenv
+	fi
 	[ -x "$(which git)" ] || sudo apt-get install git
 	echo 'Cloning rbenv source repository...'
 	git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
